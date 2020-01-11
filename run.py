fad23d8fb171a22b557b1f58b11cb1a3eb9bf94b
FernCarrera <carrerafct@gmail.com>
2020-01-10 19:56:24 -0800

    Simulation is running, need implement control

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ from ukf import *
 import pdb
 
 fig = plt.figure()
+ax = fig.add_subplot(111)
 def plot_track(track):
     ''' draw the path that the vehicle took'''
     track = np.array(track)
@@ -35,6 +36,14 @@ path = make_map(map_,nlandmarks)
 start_x = map_[0][0]
 start_y = map_[1][0]
 goal = [ map_[0][-1], map_[1][-1] ]
+
+dt = 0.1
+u = [0,0]
+sim_pos = [0,0,0]
+ellipse_step = 20
+sigma_range = 0.3; sigma_bearing = 0.1
+sigmas = [sigma_range,sigma_bearing]
+step = 10
 
 lines = states_to_track(1,['red'],['vehicle'])
 def init():
@@ -60,7 +69,21 @@ ukf.P = np.diag([0.1,0.1,0.5])
 ukf.R = np.diag([sigma_range**2, sigma_bearing**2]*nlandmarks)
 ukf.Q = np.eye(3)*0.0001
 
+sim_pos = list(ukf.x.copy())  # store position of vehicle
+sigmas = [sigma_range,sigma_bearing]    # store sensor variances
 
+# set up path dictionary 
+x_dict = [round(x,3) for x in path[0]]
+y_dict = [round(y,3) for y in path[1]]
+path_dict = dict((x,y) for x,y in zip(x_dict,y_dict) )
+# get position of landmarks
+lmark_pos = make_map.landmarks
+
+
+# define commands
+u = [0,0]
+counts = 100
+   
 
 def localization(map_,nlandmarks,sigma_vel,sigma_steer,
                     sigma_range,sigma_bearing,ellipse_step=1,step=10):
@@ -121,69 +144,60 @@ def localization(map_,nlandmarks,sigma_vel,sigma_steer,
         
 vehicle_pos = []    
 
-def animate(iterations,dt,u,sim_pos,step,ellipse_step,ukf,sigmas,lmark_pos,goal,path_dict):
+def animate(i,dt,u,sim_pos,step,ellipse_step,ukf,sigmas,lmark_pos,goal,path_dict):
     """ Run simulation """
-    for i in range(iterations):
 
-        # move vehicle
-        sim_pos = move(sim_pos,dt/step,u,wheelbase=0.5)
-        
-        # do process model prediction
-        ukf.predict(u=u,wheelbase=0.5)
+    # move vehicle
+    sim_pos = move(sim_pos,dt/step,u,wheelbase=0.5)
+    
+    # do process model prediction
+    ukf.predict(u=u,wheelbase=0.5)
 
-        # plot confidence ellipse after process model
-        if i % ellipse_step == 0:
-            plot_covariance_ellipse(
+    # plot confidence ellipse after process model
+    if i % ellipse_step == 0:
+        plot_covariance_ellipse(
+                (ukf.x[0], ukf.x[1]), ukf.P[0:2, 0:2], std=6,
+                    facecolor='k', alpha=0.3)
+
+    # store current position    
+    state = ukf.x
+    
+    #if([state[0:2]] == goal):
+    #    print("goal")
+    #    return ukf
+    z = []
+    z.extend(distance_to(lmark_pos,state,sigmas[0],sigmas[1])) 
+    
+    # distance and heading of next point to track
+    track = point_to_follow(state,path_dict,sigmas[0],sigmas[1])
+    
+    # update heading
+    u = [1,track[1]]    
+
+    # update the estimated position
+    ukf.update(z,landmarks=lmark_pos)
+
+    # plot confidence ellipse after sensor reading and kalman gain
+    if i % ellipse_step == 0:
+                plot_covariance_ellipse(
                     (ukf.x[0], ukf.x[1]), ukf.P[0:2, 0:2], std=6,
-                     facecolor='k', alpha=0.3)
+                    facecolor='g',alpha=0.8)
 
-        # store current position    
-        state = ukf.x
-        
-        if([state[0:2]] == goal):
-            print("goal")
-            return ukf
-        z = []
-        z.extend(distance_to(lmark_pos,state,sigmas[0],sigmas[1])) 
-        
-        # distance and heading of next point to track
-        track = point_to_follow(state,path_dict,sigmas[0],sigmas[1])
-        
-        # update heading
-        u = [1,track[1]]    
+    vehicle_pos.append([ukf.x[0],ukf.x[1]])
+    #print(i)
 
-        # update the estimated position
-        ukf.update(z,landmarks=lmark_pos)
-
-        # plot confidence ellipse after sensor reading and kalman gain
-        if i % ellipse_step == 0:
-                    plot_covariance_ellipse(
-                        (ukf.x[0], ukf.x[1]), ukf.P[0:2, 0:2], std=6,
-                        facecolor='g',alpha=0.8)
-
-        vehicle_pos.append([ukf.x[0],ukf.x[1]])
-
-        for t,line in enumerate(lines):
-            line.set_data(vehicle_pos[t][0],vehicle_pos[t][1])
-            return lines
+    for t,line in enumerate(lines):
+        line.set_data(vehicle_pos[t][0],vehicle_pos[t][1])
+        return lines
 
 
 
 
 
-dt = 0.1
-u = [0,0]
-sim_pos = [0,0,0]
-ellipse_step = 20
-sigma_range = 0.3; sigma_bearing = 0.1
-sigmas = [sigma_range,sigma_bearing]
 
 fargs = [dt,u,sim_pos,step,ellipse_step,ukf,sigmas,lmark_pos,goal,path_dict]
-ani = FuncAnimation(fig,animate,frames=201,interval=dt,init_func=init,repeat=False)
-
-# ---------
-sensor_std = 2.
-#track, zs = generate_data(50, sensor_std)
+ani = FuncAnimation(fig,animate,frames=2000,interval=1,repeat=False,fargs= fargs)
+plt.show()
 
 
 
